f4d5847dd632d27869f7d427fd4ed9a27749afdd
Pascal <148057928+PascalAllekotte@users.noreply.github.com>
2024-04-25 14:05:24 +0200

    test nur

---
+++ Market/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Market
+//
+//  Created by Pascal Allekotte on 25.04.24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Button(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/){
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
