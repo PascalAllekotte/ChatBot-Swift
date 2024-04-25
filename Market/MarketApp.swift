f7c5a9f932634c8be5281fad4b700bb0242aadbb
Pascal <148057928+PascalAllekotte@users.noreply.github.com>
2024-04-25 13:39:01 +0200

    Initial Commit

---
+++ Market/MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketApp.swift
+//  Market
+//
+//  Created by Pascal Allekotte on 25.04.24.
+//
+
+import SwiftUI
+
+@main
+struct MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
